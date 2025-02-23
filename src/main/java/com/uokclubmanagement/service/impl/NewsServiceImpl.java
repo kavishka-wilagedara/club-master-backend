50276d35d5d37403d8b708fd47ae7699925f665d
kavishka madhumal <wilagedarakavishka@gmail.com>
2025-02-23 19:19:22 +0530

    remove unnecessary files to clean up the project

--- src/main/java/com/uokclubmanagement/service/impl/NewsServiceImpl.java
+++ src/main/java/com/uokclubmanagement/service/impl/NewsServiceImpl.java
@@ -2,6 +2,8 @@ package com.uokclubmanagement.service.impl;
 
 import com.uokclubmanagement.entity.*;
 import com.uokclubmanagement.repository.ClubAdminRepository;
+import com.uokclubmanagement.repository.ClubRepository;
+import com.uokclubmanagement.repository.MemberRepository;
 import com.uokclubmanagement.repository.NewsRepository;
 import com.uokclubmanagement.service.NewsService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,9 +11,9 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
 import static com.uokclubmanagement.service.impl.EventServiceImpl.contentScheduleUpdating;
 
 
@@ -23,17 +25,30 @@ public class NewsServiceImpl implements NewsService {
     @Autowired
     private ClubAdminRepository clubAdminRepository;
     @Autowired
+    private ClubRepository clubRepository;
+    @Autowired
     private SequenceGeneratorService sequenceGeneratorService;
     @Autowired
-    private EventServiceImpl eventServiceImpl;
+    private MemberRepository memberRepository;
 
     @Override
     public News createNews(News news, String clubId, String clubAdminId) {
 
-        // Validate clubAdminId and clubId
-        ClubAdmin clubAdmin = eventServiceImpl.validateClubAdminAndClub(clubAdminId, clubId);
+        // Find club and clubAdmin are exist
+        Optional<ClubAdmin> clubAdminOptional = clubAdminRepository.findById(clubAdminId);
+        Optional<Club> clubOptional = clubRepository.findById(clubId);
 
-        // Check clubAdmin exist the clubId
+        if(clubAdminOptional.isEmpty()){
+            throw new RuntimeException("Invalid Club Admin");
+        }
+
+        else if(clubOptional.isEmpty()){
+            throw new RuntimeException("Invalid Club ID");
+        }
+
+        // Get ClubAdmin
+        ClubAdmin clubAdmin = clubAdminOptional.get();
+
         if (!clubAdmin.getClubId().equals(clubId)){
             throw new RuntimeException("Club ID does not match with Club Admin ID");
         }
@@ -125,16 +140,46 @@ public class NewsServiceImpl implements NewsService {
         }
     }
 
+    private News validateClubIdWithNewsAndMembers(String newsId, String clubId, String memberId) {
+
+        Optional<News> optionalNews = newsRepository.findById(newsId);
+        Optional<Club> clubOptional = clubRepository.findById(clubId);
+        Optional<Member> memberOptional = memberRepository.findById(memberId);
+
+        if(optionalNews.isEmpty()) {
+            throw new RuntimeException("Invalid News ID");
+        }
+        else if(clubOptional.isEmpty()){
+            throw new RuntimeException("Invalid Club ID");
+        }
+        else if(memberOptional.isEmpty()){
+            throw new RuntimeException("Invalid Member ID");
+        }
+        else if(!optionalNews.get().getResponseClub().equals(clubId) || !memberOptional.get().getAssociatedClubs().contains(clubId)){
+            throw new RuntimeException("Club ID error");
+        }
+
+        return optionalNews.get();
+    }
+
     @Override
-    public List<News> getAllNewsByClubId(String clubId) {
-        {
-            Optional<News> findNews = newsRepository.findById(clubId);
-            if (findNews.isPresent()) {
-                return newsRepository.getAllNewsByResponseClub(clubId);
+    public List<News> getAllNewsByMemberId(String memberId) {
+        Optional<Member> findMember = memberRepository.findById(memberId);
+        if (findMember.isPresent()) {
+            Member exisitingMember = findMember.get();
+            List<String> assigningClubs = exisitingMember.getAssociatedClubs();
+            List<News> newsList = new ArrayList<>();
+
+            for(int i = 0; i < assigningClubs.size(); i++){
+                String clubId = assigningClubs.get(i);
+
+                List<News> addNews = newsRepository.getAllNewsByResponseClub(clubId);
+                newsList.addAll(addNews);
             }
-            else {
-                throw new RuntimeException("Invalid Club ID: "+clubId);
-            }
+            return newsList;
+        }
+        else {
+            throw new RuntimeException("Invalid Member ID: "+memberId);
         }
     }
 }
