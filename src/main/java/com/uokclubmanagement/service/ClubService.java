2d9c4f39edcbcee6130cc36a8b2ceb662dbd467b
kavishka madhumal <wilagedarakavishka@gmail.com>
2025-03-04 23:26:24 +0530

    add Jackson configuration for JavaTimeModule support

--- src/main/java/com/uokclubmanagement/service/ClubService.java
+++ src/main/java/com/uokclubmanagement/service/ClubService.java
@@ -6,7 +6,6 @@ import com.uokclubmanagement.entity.Member;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.List;
 
 public interface ClubService {
